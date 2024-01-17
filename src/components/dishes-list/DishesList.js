ea806d7a2ff841e2c37ccacf67daa1515a8876fd
golib <golib@gmail.com>
2024-01-17 10:40:01 +0500

    my dishes web app

---
+++ src/components/dishes-list/DishesList.js
@@ -0,0 +1,16 @@
+import DishesItem from "../dishes-item/DishesItem";
+import Styles from "./dishes-list.module.css";
+
+function DishesList({ data }) {
+  console.log(data);
+  return (
+    <div className="container">
+      <div className={Styles.dishesCards}>
+        {data.map((dish) => (
+          <DishesItem key={dish.id} {...dish} />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default DishesList;
